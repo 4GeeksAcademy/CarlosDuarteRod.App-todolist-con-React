6329acc224f4eb1a6fbf8f803c4544c90125db5d
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-07-19 12:42:35 +0000

    Proyecto final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+	const [frase, setFrase] = useState("");
+	const [tarea, setTarea] = useState(["Tarea1", "Tarea2", "Tarea3", "Tarea4"]);
+
+	
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setTarea([...tarea, frase]);
+		setFrase("");
+	}
+
+	const handleCancel = (element) => {
+		// console.log (linea);
+		// let lista1 = tarea.filter((item) => item !== element )
+		// console.log (lista1, tarea);
+		// setTarea(lista1);
+		// console.log (lista1, tarea);
+		setTarea(tarea.filter((item) => item !== element))
+	}
+
+	return(
+		<div>
+		<form onSubmit={handleSubmit}>
+			<input type="text" value={frase} onChange={e => setFrase(e.target.value)}/>
+		</form>
+		<ul> {tarea.map((item) => <li onClick={ () => handleCancel(item)}>  {item}  </li>)}
+		{/* {tarea.map((item) => {return (<li> {item} </li>)})} */}
+		</ul>
 		</div>
-	);
+		
+	);		
 };
 
 export default Home;
