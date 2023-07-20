1e6657b4c06fdb21f7e25c638e201adf6b37f4ef
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-07-20 12:02:48 +0000

    Proyecto final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -31,9 +31,12 @@ const Home = () => {
 		<form onSubmit={handleSubmit}>
 			<input type="text" value={frase} onChange={e => setFrase(e.target.value)}/>
 		</form>
-		<ul> {tarea.map((item) => <li>  {item} <button onClick={ () => handleCancel(item)} type="button" class="btn-close" aria-label="Close"></button> </li> )} 
+		<ul> {tarea.map((item) => <li>  {item} <button onClick={ () => handleCancel(item)} type="button" class="btn-close" aria-label="Close" ></button> </li> )} 
 		{/* {tarea.map((item) => {return (<li> {item} </li>)})} */}
 		</ul>
+		<footer>
+			{tarea.length} {"item left"}
+		</footer>
 		</div>
 		
 	);		
